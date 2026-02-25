0f939d1e2fe334a854601f6d67f826fb23a8a5be
rpaana <rpaanane21@student.oulu.fi>
2026-02-25 23:31:17 +0200

    Refactor hand/animator scripts and update assets
    Rename LeftHand.controller to HandAnimator.controller and tweak animator settings (looping, blend-tree speeds, condition from Trigger to Grip). Remove Thumb.avatar mask and enable thumb bones in Trigger.mask. Delete legacy CustomGrab script and add Hand and HandController scripts that use the InputSystem, expose smooth animation speed, and handle grabbing logic. Update SampleScene and controller prefab references (assign animator controller, set hand speeds, update action references and active flags). Minor volume/profile and Mobile_RPAsset setting adjustments (version bump and various rendering/profile flags). These changes consolidate hand logic, enable smoother hand animations, and align scene assets with the new input-driven hand controller.

--- Intro_To_XR_HW3/Assets/Scripts/HandController.cs
+++ Intro_To_XR_HW3/Assets/Scripts/HandController.cs
@@ -1,16 +1,118 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class HandController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Hand hand;
+    HandController otherHand = null;
+
+    public InputActionReference gripAction;
+    public InputActionReference triggerAction;
+    public List<Transform> nearObjects = new List<Transform>();
+    public Transform grabbedObject = null;
+    public InputActionReference grabAction;
+    public bool doubleRotationEnabled = false;
+    public InputActionReference toggleRotationAction;
+    bool grabbing = false;
+    private Vector3 lastPosition;
+    private Quaternion lastRotation;
+
+    private void Start()
     {
-        
+        toggleRotationAction.action.performed += ctx => doubleRotationEnabled = !doubleRotationEnabled;
+
+        // Find the other hand
+        foreach(HandController c in transform.parent.GetComponentsInChildren<HandController>())
+        {
+            if (c != this)
+                otherHand = c;
+        }
+    }
+
+    void OnEnable()
+    {
+        grabAction.action.Enable();
+        toggleRotationAction.action.Enable();
+        gripAction.action.Enable();
+        triggerAction.action.Enable();
+    }
+
+    void OnDisable()
+    {
+        grabAction.action.Disable();
+        toggleRotationAction.action.Disable();
+        gripAction.action.Disable();
+        triggerAction.action.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        hand.SetGrip(gripAction.action.ReadValue<float>());
+        hand.SetTrigger(triggerAction.action.ReadValue<float>());
+        Grabbing();  
     }
+
+    private void Grabbing()
+    {
+        grabbing = grabAction.action.IsPressed();
+
+        Vector3 deltaPos = transform.position - lastPosition;
+        Quaternion deltaRot = transform.rotation * Quaternion.Inverse(lastRotation);
+
+        if (grabbing)
+        {
+            // Grab nearby object or the object in the other hand
+            if (!grabbedObject)
+                grabbedObject = nearObjects.Count > 0 ? nearObjects[0] : otherHand.grabbedObject;
+
+            if (grabbedObject)
+            {
+                // Change these to add the delta position and rotation instead
+                // Save the position and rotation at the end of Update function, so you can compare previous pos/rot to current here
+                grabbedObject.position += deltaPos;
+                Vector3 offset = grabbedObject.position - transform.position;
+                if (doubleRotationEnabled)
+                {
+                    deltaRot.ToAngleAxis(out float angle, out Vector3 axis);
+                    deltaRot = Quaternion.AngleAxis(angle * 2f, axis);
+                }
+                    
+                grabbedObject.position = transform.position + (deltaRot * offset);
+                grabbedObject.rotation = deltaRot * grabbedObject.rotation;
+            }
+        }
+        // If let go of button, release object
+        else if (grabbedObject)
+            grabbedObject = null;
+
+        // Should save the current position and rotation here
+        lastPosition = transform.position;
+        lastRotation = transform.rotation; 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Make sure to tag grabbable objects with the "grabbable" tag
+        // You also need to make sure to have colliders for the grabbable objects and the controllers
+        // Make sure to set the controller colliders as triggers or they will get misplaced
+        // You also need to add Rigidbody to the controllers for these functions to be triggered
+        // Make sure gravity is disabled though, or your controllers will (virtually) fall to the ground
+
+        Transform t = other.transform;
+        if(t && t.tag.ToLower()=="grabbable")
+            nearObjects.Add(t);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Transform t = other.transform;
+        if( t && t.tag.ToLower()=="grabbable")
+            nearObjects.Remove(t);
+    }
+
+    
 }
