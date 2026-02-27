80669823c48136555057fa3347730e164f0fc61d
rpaana <rpaanane21@student.oulu.fi>
2026-02-27 16:25:52 +0200

    Add Player & XR Rig prefabs, remove MainScene
    Add Player.prefab and 'XR Origin (XR Rig).prefab' under Assets/Prefabs (with associated .meta files). Remove Assets/Scenes/MainScene.unity and rename its .meta to Assets/Prefabs.meta. Also update SampleScene.unity, modify Assets/Scripts/Hand.cs, update Packages/manifest.json and packages-lock.json, and adjust ProjectSettings/TimeManager.asset.

--- Intro_To_XR_HW3/Assets/Scripts/Hand.cs
+++ Intro_To_XR_HW3/Assets/Scripts/Hand.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
-    public float speed;
-    Animator animator;
-    private float gripTarget;
-    private float triggerTarget;
-    private float gripCurrent;
-    private float triggerCurrent;
-    private string animatorGripParam = "Grip";
-    private string animatorTriggerParam = "Trigger";
+    //Animation
+    [SerializeField] private float animationSpeed;
+    private Animator _animator;
+    private float _gripTarget;
+    private float _triggerTarget;
+    private float _gripCurrent;
+    private float _triggerCurrent;
+    private const string AnimatorGripParam = "Grip";
+    private const string AnimatorTriggerParam = "Trigger";
+    private static readonly int Grip = Animator.StringToHash(AnimatorGripParam);
+    private static readonly int Trigger = Animator.StringToHash(AnimatorTriggerParam);
+
+    //Physics Movement
+    [SerializeField] private GameObject followObject;
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private Vector3 positionOffset;
+    [SerializeField] private Vector3 rotationOffset;
+    private Transform _followTarget;
+    private Rigidbody _rigidbody;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator = GetComponent<Animator>();
+        // Animation
+        _animator = GetComponent<Animator>();
+
+        // Physics Movement
+        _followTarget = followObject.transform;
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        _rigidbody.interpolation = RigidbodyInterpolation.Interpolate;
+        _rigidbody.mass = 20f;
+
+        // Teleport hands
+        _rigidbody.position = _followTarget.position;
+        _rigidbody.rotation = _followTarget.rotation;
     }
 
     // Update is called once per frame
@@ -26,28 +50,51 @@ public class Hand : MonoBehaviour
         AnimateHand();
     }
 
+    void FixedUpdate()
+    {
+        PhysicsMove();
+    }
+
+    private void PhysicsMove()
+    {
+        // Position
+        var rotatedPositionOffset = _rigidbody.rotation * positionOffset;
+        var positionWithOffset = _followTarget.position + rotatedPositionOffset;
+        var distance = Vector3.Distance(positionWithOffset, transform.position);
+        _rigidbody.linearVelocity = (positionWithOffset - transform.position).normalized * (followSpeed * distance);
+        // var positionWithOffset = _followTarget.position + (_followTarget.rotation * positionOffset);
+        // _rigidbody.MovePosition(Vector3.MoveTowards(_rigidbody.position, positionWithOffset, followSpeed * Time.fixedDeltaTime));
+
+        // Rotation
+        var rotationWithOffset = _followTarget.rotation * Quaternion.Euler(rotationOffset);
+        var rotationDifference = rotationWithOffset * Quaternion.Inverse(_rigidbody.rotation);
+        rotationDifference.ToAngleAxis(out float angle, out Vector3 axis);
+        _rigidbody.angularVelocity = axis * (angle * Mathf.Deg2Rad * rotateSpeed);
+        // _rigidbody.MoveRotation(Quaternion.RotateTowards(_rigidbody.rotation, rotationWithOffset, rotateSpeed * Time.fixedDeltaTime));
+    }
+
     internal void SetGrip(float v)
     {
-        gripTarget = v;
+        _gripTarget = v;
 
     }
 
     internal void SetTrigger(float v)
     {
-        triggerTarget = v;
+        _triggerTarget = v;
     }
 
     void AnimateHand()
     {
-        if (gripCurrent != gripTarget)
+        if (_gripCurrent != _gripTarget)
         {
-            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
-            animator.SetFloat(animatorGripParam, gripCurrent);
+            _gripCurrent = Mathf.MoveTowards(_gripCurrent, _gripTarget, Time.deltaTime * animationSpeed);
+            _animator.SetFloat(Grip, _gripCurrent);
         }
-        if (triggerCurrent != triggerTarget)
+        if (_triggerCurrent != _triggerTarget)
         {
-            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
-            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+            _triggerCurrent = Mathf.MoveTowards(_triggerCurrent, _triggerTarget, Time.deltaTime * animationSpeed);
+            _animator.SetFloat(Trigger, _triggerCurrent);
         }
     }
 
